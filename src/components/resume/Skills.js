bd8f2f19f6c2ce189ac81f2df9099afa4346a551
Aastha Naik <127818024+aasthanaik0507@users.noreply.github.com>
2023-03-14 17:41:18 +0530

    siklls

--- src/components/resume/Skills.js
+++ src/components/resume/Skills.js
@@ -62,7 +62,7 @@ const Skills = () => {
                 initial={{ x: "-100%", opacity: 0 }}
                 animate={{ x: 0, opacity: 1 }}
                 transition={{ duration: 0.5, delay: 0.5 }}
-                className="w-[850%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+                className="w-[85%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
               >
                 <span className="absolute -top-7 right-0">85%</span>
               </motion.span>
